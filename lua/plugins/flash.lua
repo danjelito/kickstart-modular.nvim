eb8d9a91b5177cfe5f465dda31182a055163715d
Devan Anjelito <anjelitodevan@gmail.com>
2025-09-16 18:54:22 +0700

    add flash function to behave like hop

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,4 +1,3 @@
--- Reference: https://github.com/folke/flash.nvim
 return {
 	"folke/flash.nvim",
 	event = "VeryLazy",
@@ -6,12 +5,62 @@ return {
 	opts = {
 		-- jump = { autojump = true },
 	},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash [s]earch" },
-    -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter [S]earch" },
-    -- { "r", mode = "o", function() require("flash").remote() end, desc = "[r]emote Flash" },
-    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+	keys = {
+		{
+			"s",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash [s]earch",
+		},
+		{
+			"<C-s>",
+			mode = { "n", "x", "o" },
+			function()
+				local Flash = require "flash"
+
+				---@param opts Flash.Format
+				local function format(opts)
+					return {
+						{ opts.match.label1, "FlashMatch" },
+						{ opts.match.label2, "FlashLabel" },
+					}
+				end
+
+				Flash.jump {
+					search = { mode = "search" },
+					label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+					pattern = [[\<]],
+					action = function(match, state)
+						state:hide()
+						Flash.jump {
+							search = { max_length = 0 },
+							highlight = { matches = false },
+							label = { format = format },
+							matcher = function(win)
+								return vim.tbl_filter(function(m)
+									return m.label == match.label and m.win == win
+								end, state.results)
+							end,
+							labeler = function(matches)
+								for _, m in ipairs(matches) do
+									m.label = m.label2
+								end
+							end,
+						}
+					end,
+					labeler = function(matches, state)
+						local labels = state:labels()
+						for m, match in ipairs(matches) do
+							match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+							match.label2 = labels[(m - 1) % #labels + 1]
+							match.label = match.label1
+						end
+					end,
+				}
+			end,
+			desc = "Flash custom two-step jump",
+		},
+	},
 }
