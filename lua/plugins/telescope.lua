fb8cab064e48c4fd54e2b917690227e2e9e5b031
Devan Anjelito <anjelitodevan@gmail.com>
2025-09-16 18:55:00 +0700

    add neotree and telescope keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,9 +21,28 @@ return {
 						require("telescope.themes").get_dropdown(),
 					},
 				},
+				pickers = {
+					buffers = {
+						sort_mru = true,
+						ignore_current_buffer = true,
+						previewer = false,
+						theme = "dropdown",
+					},
+				},
 				defaults = {
+					layout_strategy = "vertical",
 					layout_config = {
-						vertical = { width = 0.5 },
+						vertical = { width = 0.8 },
+					},
+					mappings = {
+						i = {
+							["<C-j>"] = require("telescope.actions").move_selection_next,
+							["<C-k>"] = require("telescope.actions").move_selection_previous,
+						},
+						n = {
+							["<C-j>"] = require("telescope.actions").move_selection_next,
+							["<C-k>"] = require("telescope.actions").move_selection_previous,
+						},
 					},
 				},
 			}
@@ -36,9 +55,11 @@ return {
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
 			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+			vim.keymap.set("n", '<leader>s"', builtin.registers, { desc = '[S]earch ["] Register' })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			vim.keymap.set("n", "<leader>sm", builtin.marks, { desc = "[S]earch [M]arks" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
@@ -46,6 +67,12 @@ return {
 			vim.keymap.set("n", "<leader>sn", function()
 				builtin.find_files { cwd = vim.fn.stdpath "config" }
 			end, { desc = "[S]earch [N]eovim files" })
+			-- Shortcut for searching files inside your Obsidian vault
+			vim.keymap.set("n", "<leader>so", function()
+				builtin.find_files {
+					cwd = vim.fn.expand "~/Obsidian/obsidian-vault",
+				}
+			end, { desc = "[S]earch [O]bsidian vault" })
 		end,
 	},
 }
